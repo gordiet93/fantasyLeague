8081d21bd7e7202fff256653183b691870094af7
Gordon Taggart <gordietaggart25@msn.com>
2023-10-29 20:15:51 +0000

    Frist commit

---
+++ src/main/java/com/example/fantasyLeague/FantasyLeagueApplication.java
@@ -0,0 +1,14 @@
+package com.example.fantasyLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyLeagueApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/fantasyLeague/FantasyLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasyLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
