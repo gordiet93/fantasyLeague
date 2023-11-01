331668a5d975d1bbc8cb3339f9fe57104ba24dc8
Gordon Taggart <gordietaggart25@msn.com>
2023-11-01 10:45:50 +0000

    add helper class

---
+++ src/main/java/com/example/fantasyLeague/utilities/Helper.java
@@ -0,0 +1,35 @@
+package com.example.fantasyLeague.utilities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Helper {
+
+    private static final String URLBASE = "https://soccer.sportmonks.com/api/v2.0/";
+    private static final String APITOKEN = "jbGDcAS7dcekbiqJn9HKU6pTftzFXl0dxVxQkVktGvjw1cJudCnS7p9U5pP2";
+
+    public static String callURL(String url1) throws IOException {
+        String contentString;
+        URL url = new URL(URLBASE + url1 + APITOKEN);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        //con.setRequestProperty("X-Auth-Token", "bc2a33c0a22244ce83c272a2e9562655");
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+
+        contentString = content.toString();
+
+        return contentString;
+    }
+}
